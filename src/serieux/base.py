61748d1f131ea8a640500a41ce89be96726c387e
Olivier Breuleux <breuleux@gmail.com>
2025-03-24 16:30:14 -0400

    Extract common behavior to base.py

---
+++ src/serieux/base.py
@@ -0,0 +1,109 @@
+from types import NoneType
+from typing import Any, get_args
+
+from ovld import (
+    Code,
+    CodegenInProgress,
+    Lambda,
+    OvldPerInstanceBase,
+    code_generator,
+    current_code,
+    ovld,
+    recurse,
+)
+from ovld.types import All
+
+from .model import model
+from .state import State, empty
+
+
+def standard_code_generator(fn):
+    fn.standard_codegen = True
+    return code_generator(fn)
+
+
+class BaseTransformer(OvldPerInstanceBase):
+    validate_by_default = True
+
+    def __init__(self, validate=None):
+        self.validate = self.validate_by_default if validate is None else validate
+
+    @classmethod
+    def ovld_instance_key(cls, validate=None):
+        validate = cls.validate_by_default if validate is None else validate
+        return (("this", cls), ("validate", validate))
+
+    ###########
+    # helpers #
+    ###########
+
+    def embed_condition(self, t):
+        if t in (int, str, bool, float, NoneType):
+            return t
+
+    def subcode(self, t, accessor, state_t, after=None, default=True, objt=None):
+        if objt is not None:
+            ec = objt
+        elif ec := self.embed_condition(t):
+            ec = All[ec]
+        if ec is not None:
+            try:
+                fn = self.transform.resolve(type[t], ec, state_t, after=after)
+                lbda = getattr(fn, "__lambda__", None)
+                if lbda:
+                    return lbda(None, t, accessor, "$state")
+            except CodegenInProgress:
+                pass
+        return self.default_codegen(t, accessor) if default else None
+
+    def guard_codegen(self, t, accessor, body):
+        if not self.validate:
+            return Code(body)
+        else:
+            return Code(
+                "$body if isinstance($accessor, $t) else $dflt",
+                body=Code(body),
+                accessor=Code(accessor),
+                t=t,
+                dflt=self.default_codegen(t, accessor),
+            )
+
+    def default_codegen(self, t, accessor):
+        return Code(
+            "$recurse($t, $accessor, $state)",
+            t=t,
+            accessor=accessor if isinstance(accessor, Code) else Code(accessor),
+            recurse=self.transform,
+        )
+
+    ######################
+    # transform: codegen #
+    ######################
+
+    @ovld(priority=1)
+    @standard_code_generator
+    def transform(self, t: type[object], obj: Any, state: State, /):
+        (t,) = get_args(t)
+        mt = model(t)
+        if mt == t:
+            return None
+        else:
+            code = self.subcode(mt, "$obj", state, after=current_code, objt=obj)
+            return code and Lambda(code)
+
+    for T in (int, str, bool, float, NoneType):
+
+        @standard_code_generator
+        def transform(self, t: type[T], obj: T, state: State, /):
+            (t,) = get_args(t)
+            return Lambda(self.guard_codegen(t, "$obj", "$obj"))
+
+    ###########################
+    # transform: entry points #
+    ###########################
+
+    def transform(self, obj: object, /):
+        return recurse(type(obj), obj, empty)
+
+    def transform(self, t: type[object], obj: object, /):
+        return recurse(t, obj, empty)
--- src/serieux/serialization.py
+++ src/serieux/serialization.py
@@ -1,100 +1,30 @@
-from types import NoneType
 from typing import Any, get_args, get_origin
 
-from ovld import (
-    Code,
-    CodegenInProgress,
-    Lambda,
-    OvldPerInstanceBase,
-    current_code,
-    ovld,
-    recurse,
-    subclasscheck,
-)
-from ovld.types import All
+from ovld import Code, Lambda, extend_super, subclasscheck
 
-from .model import Modell as Model, model
-from .state import State, empty
-from .transform import standard_code_generator
+from .base import BaseTransformer, standard_code_generator
+from .model import Modell as Model
+from .state import State
 from .typetags import strip_all
 from .utils import UnionAlias
 
 
-class Serializer(OvldPerInstanceBase):
-    validate_by_default = True
-
-    def __init__(self, validate=None):
-        self.validate = self.validate_by_default if validate is None else validate
-
-    @classmethod
-    def ovld_instance_key(cls, validate=None):
-        validate = cls.validate_by_default if validate is None else validate
-        return (("this", cls), ("validate", validate))
-
+class Serializer(BaseTransformer):
     ###########
     # helpers #
     ###########
 
-    def valtype(self, t):
-        if subclasscheck(t, UnionAlias):
-            return All
-        else:
-            return All[t]
-
-    def subcode(self, t, accessor, state_t, after=None, default=True):
-        try:
-            fn = self.transform.resolve(type[t], self.valtype(t), state_t, after=after)
-            lbda = getattr(fn, "__lambda__", None)
-            if lbda:
-                return lbda(None, t, accessor, "$state")
-        except CodegenInProgress:
-            pass
-        return self.default_codegen(t, accessor) if default else None
-
-    def default_codegen(self, t, accessor):
-        return Code(
-            "$recurse($t, $accessor, $state)",
-            t=t,
-            accessor=Code(accessor),
-            recurse=self.transform,
-        )
-
-    def guard_codegen(self, t, accessor, body):
-        if not self.validate:
-            return Code(body)
-        else:
-            return Code(
-                "$body if isinstance($accessor, $t) else $dflt",
-                body=Code(body),
-                accessor=Code(accessor),
-                t=t,
-                dflt=self.default_codegen(t, accessor),
-            )
+    def embed_condition(self, t):
+        if not subclasscheck(t, UnionAlias):
+            return t
 
     ######################
     # transform: codegen #
     ######################
 
-    @ovld(priority=1)
+    @extend_super
     @standard_code_generator
-    def transform(self, t: type[object], obj: Any, state: State, /):
-        (t,) = get_args(t)
-        mt = model(t)
-        if mt == t:
-            return None
-        else:
-            code = self.subcode(mt, "$obj", state, after=current_code)
-            return code and Lambda(code)
-
-    for T in (int, str, bool, float, NoneType):
-
-        @standard_code_generator
-        def transform(self, t: type[T], obj: T, state: State, /):
-            (t,) = get_args(t)
-            return Lambda(self.guard_codegen(t, "$obj", "$obj"))
-
-    @standard_code_generator
-    def transform(self, t: type[Model], obj: Model, state: State, /):
+    def transform(self, t: type[Model], obj: object, state: State, /):
         (t,) = get_args(t)
         return Lambda(
             "{$[,]parts}",
@@ -140,16 +70,6 @@ class Serializer(OvldPerInstanceBase):
                 code=code,
             )
         return Lambda(code)
-
-    ###########################
-    # transform: entry points #
-    ###########################
-
-    def transform(self, obj: object, /):
-        return recurse(type(obj), obj, empty)
-
-    def transform(self, t: type[object], obj: object, /):
-        return recurse(t, obj, empty)
 
 
 default = Serializer()
