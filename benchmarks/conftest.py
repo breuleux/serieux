91668d0f6dffd16b7ca99de55d4696f8820b6f3a
Olivier Breuleux <breuleux@gmail.com>
2025-11-07 01:13:44 -0500

    Reorganize benchmark code (bis)

--- benchmarks/conftest.py
+++ benchmarks/conftest.py
@@ -3,12 +3,13 @@ from collections import defaultdict
 import pytest
 
 
-@pytest.mark.hookwrapper
+@pytest.hookimpl(hookwrapper=True)
 def pytest_benchmark_group_stats(config, benchmarks, group_by):
     outcome = yield
     results = defaultdict(list)
     for bench in benchmarks:
-        iface = bench["params"]["interface"].__name__
-        group = bench["name"].replace(iface, "").replace(",]", "]").replace("[]", "")
+        case = bench["params"]["case"]
+        an = case.adapter_name
+        group = bench["name"].replace(an, "").replace(",]", "]").replace("[]", "")
         results[group].append(bench)
     outcome.force_result(results.items())
--- benchmarks/test_all.py
+++ benchmarks/test_all.py
@@ -1,101 +1,44 @@
 from __future__ import annotations
 
-import itertools
 import json
+from pathlib import Path
 
 import pytest
 
-from serieux import serialize
+from serieux import IncludeFile, Serieux, serialize
 
-from .adapters.adaptix import AdaptixAdapter
-from .adapters.apischema import ApischemaAdapter
-from .adapters.marshmallow import MarshmallowAdapter
-from .adapters.mashumaro import MashumaroAdapter
-from .adapters.pydantic import PydanticAdapter
-from .adapters.serde import SerdeAdapter
-from .adapters.serieux import SerieuxAdapter
-from .data.point import point
-from .data.tree import tree
-from .data.world import big_world, roboland, world
+from .matrix import Matrix
 
-adaptix = AdaptixAdapter()
-adaptix.__name__ = "adaptix"
-apischema = ApischemaAdapter()
-apischema.__name__ = "apischema"
-marshmallow = MarshmallowAdapter()
-marshmallow.__name__ = "marshmallow"
-mashumaro = MashumaroAdapter()
-mashumaro.__name__ = "mashumaro"
-pydantic = PydanticAdapter()
-pydantic.__name__ = "pydantic"
-serde = SerdeAdapter()
-serde.__name__ = "serde"
-serieux = SerieuxAdapter()
-serieux.__name__ = "serieux"
+here = Path(__file__).parent
+deserialize = (Serieux + IncludeFile)().deserialize
 
 
-id_to_thing = {id(v): k for k, v in globals().items()}
+def bench(path):
+    matrix = deserialize(Matrix, path)
+    return pytest.mark.parametrize("case", matrix.generate_cases())
 
 
-def bench(interfaces, data):
-    cases = list(itertools.product(data, interfaces))
-    return pytest.mark.parametrize(
-        "data,interface",
-        cases,
-        ids=[f"{id_to_thing[id(d)]},{id_to_thing[id(i)]}" for d, i in cases],
-    )
-
-
-@bench(
-    interfaces=[
-        apischema,
-        marshmallow,
-        pydantic,
-        adaptix,
-        mashumaro,
-        serieux,
-        serde,
-    ],
-    data=[point, world, roboland, tree, big_world],
-)
-def test_serialize(interface, data, benchmark):
-    fn = interface.serializer_for_type(type(data))
-    result = benchmark(fn, data)
-    assert result == serialize(type(data), data)
-
-
-@bench(
-    interfaces=[
-        apischema,
-        marshmallow,
-        pydantic,
-        adaptix,
-        mashumaro,
-        serieux,
-        serde,
-    ],
-    data=[roboland],
-)
-def test_json(interface, data, benchmark):
-    fn = interface.json_for_type(type(data))
-    result = benchmark(fn, data)
-    assert json.loads(result) == serialize(type(data), data)
-
-
-@bench(
-    interfaces=[
-        apischema,
-        marshmallow,
-        pydantic,
-        adaptix,
-        mashumaro,
-        serieux,
-        serde,
-    ],
-    data=[point, world, roboland, tree, big_world],
-)
-def test_deserialize(interface, data, benchmark):
-    data_ser = serialize(data)
-    fn = interface.deserializer_for_type(type(data))
+@bench(here / "matrix.yaml")
+def test_serialize(case, benchmark):
+    case.xfail_guard()
+    data_ser = serialize(case.data)
+    fn = case.adapter.deserializer_for_type(type(case.data))
     result = benchmark(fn, data_ser)
-    assert result == data
+    assert result == case.data
+
+
+@bench(here / "matrix-json.yaml")
+def test_json(case, benchmark):
+    case.xfail_guard()
+    fn = case.adapter.json_for_type(type(case.data))
+    result = benchmark(fn, case.data)
+    assert json.loads(result) == serialize(type(case.data), case.data)
+
+
+@bench(here / "matrix.yaml")
+def test_deserialize(case, benchmark):
+    case.xfail_guard()
+    data_ser = serialize(case.data)
+    fn = case.adapter.deserializer_for_type(type(case.data))
+    result = benchmark(fn, data_ser)
+    assert result == case.data
